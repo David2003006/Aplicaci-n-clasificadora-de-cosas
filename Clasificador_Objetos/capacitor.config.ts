60ecf5885d13c15a02c4d2345b5a26834fba72b8
David2003006 <davidalejandro2003@yahoo.com>
2025-04-18 23:11:45 -0600

    Aplicacion movil

---
+++ Clasificador_Objetos/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Clasificador_Objetos',
+  webDir: 'www'
+};
+
+export default config;
